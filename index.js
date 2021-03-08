7a9058ea3d061c6af8d4aa00e8ef6cb90f346e94
LTH9898 <35323806+LTH9898@users.noreply.github.com>
2021-03-08 22:14:51 +0900

    //

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
-const title = document.getElementById("title");
+const title = document.querySelector("#bodytitle");
 
-console.log(title);
+const CLICKED_CLASS = "clicked";
+
+function handleClick(){
+    const currentClass = title.className;
+    console.log(currentClass);
+    if(currentClass !== CLICKED_CLASS){
+        title.className = CLICKED_CLASS;
+    } else {
+        title.className = "";
+    }
+}
+
+
+function init() {
+   // title.style.color = BASE_COLOR;
+    title.addEventListener("click", handleClick);
+}
+
+init();
+
+
+function handleOffline()
+{
+    console.log("offline");
+}
+
+
+window.addEventListener("offline", handleOffline);
+
+function handleOnline()
+{
+    console.log("online");
+}
+
+window.addEventListener("online", handleOnline);
